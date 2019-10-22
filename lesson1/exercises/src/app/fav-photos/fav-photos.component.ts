e10e8a9c271378f47403dcd49f1b34e1d1029824
Safna <safna.usa@gmail.com>
2019-10-21 19:12:02 -0500

    Completed Code

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Success';
+  photosTitle = 'Success Images';
   image1 = 'https://www.launchcode.org/assets/icons/trophy-95e8cbe9bfda44123422302951deb1c92a237d39052669b8fbfafec00cb4f608.png';
   image2 = 'https://sial.school/wp-content/uploads/2018/12/believing-in-yourself-is-the-secret-to-success.jpg';
   image3 = 'https://i.ytimg.com/vi/8D9B6TfxCCY/maxresdefault.jpg';
